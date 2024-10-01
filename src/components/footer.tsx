ded06e7b6175874d7b88ac26a34d85193e2e2f56
Arhammazhar <arhamatiq2007@gmail.com>
2024-10-02 04:07:59 +0500

    updated

---
+++ src/components/footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer(){
+    return(
+        <div className="footer">
+            <ul className="footerButtons">
+                <li>Privacy Policy</li>
+                <li>Terms of Service</li>
+            </ul>
+        </div>
+    )
+}
